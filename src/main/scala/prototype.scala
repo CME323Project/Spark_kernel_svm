8fa8328f71282e7485aaf7bfdfebb5077ac46e52
Haoming Li <haoming.li.92@gmail.com>
2015-05-25 21:05:23 -0700

    beta 0.1

--- src/main/scala/prototype.scala
+++ src/main/scala/prototype.scala
@@ -37,11 +37,12 @@ class KernelSVM(training_data:RDD[LabeledPoint], lambda_s:Double, kernel : Strin
             var sample = (working_data.takeSample(true, 1))(0)
             y = sample._2._1.label
             yp = working_data.map{case (k,v) => (v._1.label * v._2 * kernel_func.evaluate(v._1.features, sample._2._1.features))}.reduce((a, b) => a + b)
-            s = (1 - 1D/t)*s
+            s = (1 - 1D/(t+1))*s
             if (y * yp < 1) {
                 norm = norm + (2*y) / (lambda * t) * yp + math.pow((y/(lambda*t)), 2)*kernel_func.evaluate(sample._2._1.features, sample._2._1.features)
                 alpha = working_data.get(sample._1).get._2
-                working_data = working_data.put(sample._1, (sample._2._1, alpha + (y/(lambda*t*s))))
+                working_data = working_data.put(sample._1, (sample._2._1, alpha + (1/(lambda*t*s))))
+                
                 if (norm > (1/lambda)) {
                     s = s * (1/math.sqrt(lambda*norm))
                     norm = (1/lambda)
@@ -51,6 +52,8 @@ class KernelSVM(training_data:RDD[LabeledPoint], lambda_s:Double, kernel : Strin
             t = t+1
         }
         model = working_data.map{case (k, v) => (v._1, v._2)}.filter{case (k,v) => (v > 0)}
+        print (model.count())
+        println("fuck")
 
 
     }
@@ -59,9 +62,11 @@ class KernelSVM(training_data:RDD[LabeledPoint], lambda_s:Double, kernel : Strin
         s * (model.map{case (k,v) => v * k.label * kernel_func.evaluate(data.features, k.features)}.reduce((a, b) => a + b))
 
     }
-    def getAccuracy(data: RDD[LabeledPoint]): Double = {
-        val N_c = data.map(x => (predict(x) * x.label) ).filter(x => x>0).count()
-        val N = data.count()
+    def getAccuracy(data: Array[LabeledPoint]): Double = {
+        val N_c = data.map(x => (predict(x) * x.label) ).count(x => x>0)
+        val N = data.count(x => true)
+        println(N_c)
+        println(N)
         (N_c / N)
 
     }
@@ -73,13 +78,15 @@ object SimpleApp {
         val sc = new SparkContext(conf)
 
         val data =  MLUtils.loadLibSVMFile(sc, "data/a8a.txt")
+        val data_train = data.sample(false, 0.1)
 
-        val svm = new KernelSVM(data, 1.0, "rbf", 1.0, 1)
+        val svm = new KernelSVM(data_train, 1.0, "rbf", 1.0, 100)
         svm.train()
 
 
-        val test = MLUtils.loadLibSVMFile(sc, "data/a8at.txt")
-        svm.getAccuracy(test)
+        //val test = MLUtils.loadLibSVMFile(sc, "data/a8at.txt")
+        val local_test = data.takeSample(false, 1000)
+        println(svm.getAccuracy(local_test))
 
     }
 }
