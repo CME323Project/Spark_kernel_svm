2ae4a45eb9d91ef5b56bc66e4a9744655c4f984a
Bangzheng He <dhanianhe@gmail.com>
2015-06-02 01:11:36 -0700

    test

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,30 +6,47 @@ import org.apache.spark.rdd._
 import org.apache.spark.mllib.util.MLUtils
 
 import java.io._
+import java.lang.System
 
 object TestKernelSVM {
     def main(args: Array[String]) {
-        val logFile = "README.md" // Should be some file on your system
-        val conf = new SparkConf().setAppName("KernelSVM Test")
-        val sc = new SparkContext(conf)
+      
+      if (args.length != 1 ) {
+      println("Usage: /path/to/spark/bin/spark-submit --packages amplab:spark-indexedrdd:0.1" +
+        "target/scala-2.10/spark-kernel-svm_2.10-1.0.jar <data file>")
+      sys.exit(1)
+      }
+      
+      val logFile = "README.md" // Should be some file on your system
+      val conf = new SparkConf().setAppName("KernelSVM Test")
+      val sc = new SparkContext(conf)
+       
+      val data =  MLUtils.loadLibSVMFile(sc, args(0))
 
-        val data =  MLUtils.loadLibSVMFile(sc, "data/a8a.txt")
-
-        val data_train = data.sample(false, 0.8)
-
-        val svm = new KernelSVM(data_train, 1.0, "rbf", 1.0)
-        svm.train(1000,50)
-
-        //val test = MLUtils.loadLibSVMFile(sc, "data/a8at.txt")
-        val local_test = data.takeSample(false, 1000)
-
-        val pw = new PrintWriter(new File("result.txt" ))
-        pw.write(svm.getAccuracy(local_test).toString)
-        pw.write(svm.getNumSupportVectors().toString)
-        pw.close
-        println(svm.getAccuracy(local_test))
-
-        println(svm.getNumSupportVectors())
-
+      val splits = data.randomSplit(Array(0.8,0.2))
+      val training = splits(0)
+      val test = splits(1).collect()
+      
+      val m = training.count()
+      
+      var pack_size = 100
+      
+      val iterations = List((0.5*m).toLong,m.toLong,(1.5*m).toLong,(2*m).toLong)
+      var num_iter = 0
+      
+      val pw = new PrintWriter(new File("result.txt" ))
+      
+      for (num_iter <- iterations) {
+        val t1 = System.currentTimeMillis
+        val svm = new KernelSVM(training, 1.0/m, "rbf", 1.0)
+        svm.train(num_iter,pack_size)
+        val t2 = System.currentTimeMillis
+        val runtime = (t2 - t1)/1000
+        
+        var ss = m.toString + " " + num_iter.toString + " " + pack_size.toString + " " + svm.getAccuracy(test).toString + " " + runtime.toString + "\n"
+        pw.write(ss)
+      }
+      
+      pw.close
     }
 }
